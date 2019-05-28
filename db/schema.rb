c809bb48c675c65f6613872ea7f2da808011ea60
harshvijayvargiya <ror.training0@gmail.com>
2019-05-28 17:52:02 +0530

    admin pannel / bottstrap / css

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_072539) do
+ActiveRecord::Schema.define(version: 2019_05_28_121358) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -69,6 +69,13 @@ ActiveRecord::Schema.define(version: 2019_05_27_072539) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "role"
+    t.string "provider"
+    t.string "uid"
+    t.string "token"
+    t.integer "expires_at"
+    t.boolean "expires"
+    t.string "refresh_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
